99993dc294ff1da5e6288c8483e750f6781f47e7
SantiagoIvan <santiago.feijoo96@gmail.com>
2022-05-03 01:10:48 -0300

    first script

---
+++ deploy/01-deploy-governor-token.ts
@@ -0,0 +1,8 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const deployGovernanceToken: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    console.log("Im here")
+}
+
+export default deployGovernanceToken
