e84da679b391bbef5dcd532f4eed6a6d7e1f295a
SantiagoIvan <santiago.feijoo96@gmail.com>
2022-05-05 01:14:48 -0300

    un comment

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat'
 
-export const MIN_DELAY = 3600
+export const MIN_DELAY = 3600 // equivalente a una hora. Esta en segundos
 export const VOTING_DELAY = 1 // 1 blocks
 export const VOTING_PERIOD = 5 // 5 block
 export const QUORUM_PERCENTAGE = 4
