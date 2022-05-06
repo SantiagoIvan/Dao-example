553d2f92823977e717fd7ce7dbac255559e16714
SantiagoIvan <santiago.feijoo96@gmail.com>
2022-05-06 16:30:47 -0300

    Deployment tests

--- test/unit/deploy.ts
+++ test/unit/deploy.ts
@@ -7,8 +7,15 @@ import {
     TimeLock,
     Box,
 } from '../../typechain-types'
-import { assert } from 'console'
-import exp from 'constants'
+import { SignerWithAddress } from 'hardhat-deploy-ethers/signers'
+import { Contract } from 'ethers'
+import {
+    ADDRESS_ZERO,
+    MIN_DELAY,
+    QUORUM_PERCENTAGE,
+    VOTING_DELAY,
+    VOTING_PERIOD,
+} from '../../utils/utils'
 
 /**
  * `describe` es una funcion que permite organizar los tests. En realidad no es obligatoria,
@@ -26,26 +33,71 @@ describe('Set of Contracts', function () {
      * para que puedan ser accedidas despues por todos los demas bloques
      */
 
-    let daoToken: DaoToken
-    let timelock: TimeLock
-    let box: Box
-    let governor: GovernorContract
-    let deployer: string
+    let daoToken: DaoToken | Contract
+    let timelock: TimeLock | Contract
+    let box: Box | Contract
+    let governor: GovernorContract | Contract
+    let deployer: SignerWithAddress
+    let addr1: SignerWithAddress
+    let addr2: SignerWithAddress
 
     const voteType = 1 // a favor
     const reason = 'A random reason to vote'
 
     beforeEach(async function () {
-        // const daoTokenContract = await ethers.getContractFactory("DaoToken")
-        daoToken = await ethers.getContract('DaoToken')
+        const addresses = await ethers.getSigners()
+        deployer = addresses[0]
+        addr1 = addresses[1]
+        addr2 = addresses[2]
+
+        const daoTokenContract = await ethers.getContractFactory(
+            'DaoToken',
+            deployer
+        )
+        const timelockContract = await ethers.getContractFactory(
+            'TimeLock',
+            deployer
+        )
+        const governorContract = await ethers.getContractFactory(
+            'GovernorContract',
+            deployer
+        )
+        const boxContract = await ethers.getContractFactory('Box', deployer)
+
+        daoToken = await daoTokenContract.deploy()
+        timelock = await timelockContract.deploy(MIN_DELAY, [], [])
+        governor = await governorContract.deploy(
+            daoToken.address,
+            timelock.address,
+            VOTING_DELAY,
+            VOTING_PERIOD,
+            QUORUM_PERCENTAGE
+        )
+        box = await boxContract.deploy()
+        await daoToken.deployed()
+        await timelock.deployed()
+        await governor.deployed()
+        await box.deployed()
     })
 
     describe('Deployment', function () {
         it('Token deploys successfully', async function () {
-            expect(1).to.equal(2)
+            expect(daoToken.address).to.not.equal(ADDRESS_ZERO)
         })
-
-        it('Should assign the total supply of tokens to the owner', async function () {})
+        it('TimeLock deploys successfully', async function () {
+            expect(timelock.address).to.not.equal(ADDRESS_ZERO)
+        })
+        it('GovernorContract deploys successfully', async function () {
+            expect(governor.address).to.not.equal(ADDRESS_ZERO)
+        })
+        it('Box deploys successfully', async function () {
+            expect(box.address).to.not.equal(ADDRESS_ZERO)
+        })
+        it('Should assign the total supply of tokens to the owner', async function () {
+            const supply = await daoToken.s_maxSupply()
+            const balance = await daoToken.balanceOf(deployer.address)
+            expect(balance.toString()).to.equal(supply.toString())
+        })
     })
 
     describe('Transactions', function () {
